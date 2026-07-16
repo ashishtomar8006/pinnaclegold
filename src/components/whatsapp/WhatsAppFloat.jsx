32c14057c10e32b029fa877487f11ef1fd54fd75
ashishtomar8006 <ashishtomar.liangtuang@gmail.com>
2026-07-16 22:19:26 +0530

    commit

--- src/components/whatsapp/WhatsAppFloat.jsx
+++ src/components/whatsapp/WhatsAppFloat.jsx
@@ -5,7 +5,7 @@ export default function WhatsAppFloat() {
   return (
     <>
       <a
-        href="https://api.whatsapp.com/send/?phone=917230972309&text=Hi%2C+I+would+like+to+explore+Pinnacle&type=phone_number&app_absent=0"
+        href="https://api.whatsapp.com/send/?phone=917230972309&text=Connect+with+The+Pinnacle+By+STJ+on+WhatsApp&type=phone_number&app_absent=0"
         className="fixed bottom-4 left-4 z-10 flex h-16 w-16 items-center justify-center rounded-full bg-green-500 shadow-lg hover:bg-green-600"
         target="_blank"
         rel="noopener noreferrer"
